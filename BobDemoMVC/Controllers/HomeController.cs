366c580e9045c2c103ef96e8330cd51adfa0cfef
Istvan Marki <istv0050@edu.easj.dk>
2017-02-14 15:38:11 +0100

    initialize upload commit

---
+++ BobDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BobDemoMVC.Models;
+
+namespace BobDemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // Simplest way to inject data to view (not the best) is to use the viewBag with dynamic properties:
+            
+            ViewBag.MyMethod = "what is this";
+
+
+            //OR
+
+            //Make a call out
+            var cars = HelloModel.GetCars();
+
+            // returns a ViewResult object (of type: ActionResult) that renders a view to the response
+            // this case: \Views\Home\Index.cshtml
+
+            //TODO: 1 of the overload pass an "object graph" into the View #2nd way
+
+            return View(cars);
+        }
+    }
+}
---
+++ BobDemoMVC/Models/HelloModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BobDemoMVC.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            //collection initializer
+            List<Car> cars = new List<Car>
+            {
+                //object initializers
+                new Car {Id = 1, Make = "BMW", Model = "528i"},
+                new Car {Id = 1, Make = "Hunyadi", Model = "Elantra"},
+                new Car {Id = 1, Make = "Toyota", Model = "4Runner"}
+
+            };
+            return cars;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
