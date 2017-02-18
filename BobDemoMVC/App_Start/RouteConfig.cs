7f1964cdb5e2feca6026624ae3092314c63a6a03
Istvan Marki <istv0050@edu.easj.dk>
2017-02-18 14:26:31 +0100

    more comments to skim through and understand the basics

--- BobDemoMVC/App_Start/RouteConfig.cs
+++ BobDemoMVC/App_Start/RouteConfig.cs
@@ -9,13 +9,24 @@ namespace BobDemoMVC
 {
     public class RouteConfig
     {
+        //This method is being called on the Application_Start Eventhandler in the Global.asax.cs
+        
+            //passing in a collection of routes
+
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            //We are populating that RouteColection with new routes that we are creating
             routes.MapRoute(
                 name: "Default",
+                //the url input parameter defines the pattern that will be matched up.
+                //The pattern is defined by a series of in the url (segment is defined by that forward slash in the url)
                 url: "{controller}/{action}/{id}",
+
+                //ID is optional
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
--- BobDemoMVC/Global.asax.cs
+++ BobDemoMVC/Global.asax.cs
@@ -12,6 +12,9 @@ namespace BobDemoMVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            //At the very beginning of the application, when it first starts up, "I need you to go and figure it out
+            //all of the route mappings that have been defined."
+            //Because all incoming requests are gointg to have to look at a TABLE OF ROUTE PATTERNS to decide where to route that incoming request to.
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
